49b82ce962a33a8a6d5ece82e709068f25d53280
Jenpar1674 <51331242+Jenpar1674@users.noreply.github.com>
2019-08-19 07:24:35 -0500

    morning commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Search from './components/Search';
 import Navbar from './components/Navbar';
 import Photos from './components/Photos';
-//import { Grid ,Jumbotron } from 'react-bootstrap';
+
 import {Route} from 'react-router-dom'
 import axios from 'axios'
 import apiKey from './components/config';
@@ -101,9 +101,9 @@ render(){
               
       <div className="performSearch">
       
-        <Navbar onClick= {this.performSearch} />
+        {/* <Navbar onClick= {this.performSearch} /> */}
         
-          
+          <Navbar/>
             
       <Results/>  
           <Route exact path= "/butterflies" render ={ () => <Results title = "Butterflies" />} />
@@ -113,12 +113,9 @@ render(){
           <Route exact path="/search" render={ () => <Results title = "Cool Pics Below"/>} />
         
       <div className="main-content">
-      {
-        (this.state.loading)
-            ? <h3>Loading...</h3>
-            :
+      
               <Switch>
-              /* renders data to the proper url pages showing each unique image array   */
+              {/* renders data to the proper url pages showing each unique image array   */}
                 <Route exact path="/search" component={() => <Photos pics={this.state.pics} />} />
                 <Route exact path="/butterflies" component={() => <Photos pics={this.state.butterflies} />} />
                 <Route exact path="/bobcats" component={() => <Photos pics={this.state.bobcats} />} />
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,52 +1,92 @@
+//Import React library
 import React, { Component } from 'react';
-import {withRouter} from 'react-router-dom'
 
-class Search extends Component {
-  constructor(props){
-    super(props);
+export default class Search extends Component {
 
-  this.state = {
-    searchText:'',
-    loading: false
-}
+  //Gets updated with onSearchChange
+  state = {
+    searchText: ''
   }
-onSearchChange =(e)=>{
-    this.setState({
-      searchText:e.target.value,
-      loading:true
-    });
+
+  //Text user types into input field
+  onSearchChange = e => {
+    this.setState({ searchText: e.target.value });
+  }
+
+  //Called when form is submitted
+  handleSubmit = e => {
+    this.props.history.push(
+      `/search/${this.state.searchText}`
+    );
+    e.preventDefault();
+    this.props.onSearch(this.query.value);
+    e.currentTarget.reset();
+  }
+
+  render() {
+    return (
+      <form className="search-form" onSubmit={this.handleSubmit} >
+
+        <input type="search"
+               onChange={this.onSearchChange}
+               name="search"
+               ref={(input) => this.query = input}
+               placeholder="Search..." />
+        <button type="submit" id="submit" className="search-button"><i className="material-icons icn-search">search</i></button>
+      </form>
+    );
+  }
 }
-handleSubmit = (e) =>{
-  e.preventDefault();
-  let value =this.searchText.value;
-  let path= `/${value}`
-  this.props.history.push(path);
-  this.props.onSearch(value);
-  this.setState({
-    searchText:''
-  })
+
+// import React, { Component } from 'react';
+// import {withRouter} from 'react-router-dom'
+
+// class Search extends Component {
+//   constructor(props){
+//     super(props);
+
+//   this.state = {
+//     searchText:'',
+//     loading: false
+// }
+//   }
+// onSearchChange =(e)=>{
+//     this.setState({
+//       searchText:e.target.value,
+//       loading:true
+//     });
+// }
+// handleSubmit = (e) =>{
+//   e.preventDefault();
+//   let value =this.searchText.value;
+//   let path= `/${value}`
+//   this.props.history.push(path);
+//   this.props.onSearch(value);
+//   this.setState({
+//     searchText:''
+//   })
   
-     }
+//      }
 
-    render() {
-        return(
-          <form className="search-form" onSubmit={this.handleSubmit} >
-          <label className="is-hidden" htmlFor="search"></label>
-          <input type="search" 
-                 onChange={this.onSearchChange}
-                 name="search" 
-                 ref={(input)=>this.searchText=input}
-                 placeholder="Search..." />
+//     render() {
+//         return(
+//           <form className="search-form" onSubmit={this.handleSubmit} >
+//           <label className="is-hidden" htmlFor="search"></label>
+//           <input type="search" 
+//                  onChange={this.onSearchChange}
+//                  name="search" 
+//                  ref={(input)=>this.searchText=input}
+//                  placeholder="Search..." />
                  
-          <button 
-          type="submit" 
-          value="&#x1F50D;"
-          id="submit" 
-          className="search-button">
-            <i className="material-icons icn-search">search</i></button>
-        </form>      
-      );
-    }
-  }
+//           <button 
+//           type="submit" 
+//           value="&#x1F50D;"
+//           id="submit" 
+//           className="search-button">
+//             <i className="material-icons icn-search">search</i></button>
+//         </form>      
+//       );
+//     }
+//   }
 
-export default withRouter(Search)            
+// export default withRouter(Search)            
